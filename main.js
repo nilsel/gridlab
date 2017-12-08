a7686cb849a4fde8fcb9fb2896abb11d9e8012c6
Nils P. Ellingsen <q2i@storebrand.no>
2017-12-08 15:55:52 +0100

    use parcel for local dev, move script to its own file

---
+++ main.js
@@ -0,0 +1,24 @@
+
+
+$('document').ready(function(){
+
+  $('.tile').hover(
+    function(){
+      $(this).find('.tile-img').addClass('hoover');
+    },
+    function(){
+      $(this).find('.tile-img').removeClass('hoover');
+    });
+
+  $('.grid-item .tile-img').click(function(){
+    var elem = $(this).find('img');
+    elem.addClass('slide-out-blurred-top');
+    setTimeout(function(){
+      elem.removeClass('slide-out-blurred-top').addClass('slide-in-blurred-top');
+    }, 1000);
+  })
+
+  $('.grid-item strong').click(function(){
+    $(this).parent().find('.tile-img img').trigger('click');
+  })
+});
